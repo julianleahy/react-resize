b55a88a1b434351c00b729836d35e2ad200ba35b
julianleahy <julianleahynz@gmail.com>
2018-03-14 12:40:00 +1300

    add resize container class

---
+++ Resize.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Resize.css'
+
+
+class Resize extends Component {
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Resize;
